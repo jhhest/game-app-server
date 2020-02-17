f22ec1d8d4a6bbc8d35936b4b33ee5ba22559724
jhhest <janvanhest@outlook.com>
2020-02-17 13:38:48 +0100

    Create login endpoint, POST /user/login. When client logins passwordis checked and authenticed. If true client will receive 200 ok. and jwt token.

--- routes/user/router.js
+++ routes/user/router.js
@@ -1,17 +1,24 @@
+/**
+ * FIXME: Username exits.
+ * FIXME: EMAIL Taken.
+ */
+
 const { Router } = require("express");
 const bcrypt = require("bcrypt");
+const { toJWT } = require("../../auth/jwt");
 
 const router = new Router();
-
 const User = require("./model");
 
+// find all users. Just a test.
 router.get("/user", (request, response, next) =>
   User.findAll()
-    .then(user => response.send(user)) //  TODO: Do do we need user.json?
+    .then(user => response.send(user))
     .catch(error => next(error))
 );
 
 // http :5000/user username=jan email=jan@vanhest.work password=password
+// Create a users.
 router.post("/user", (request, response, next) => {
   const { username, email, password } = request.body;
   User.create({
@@ -21,5 +28,17 @@ router.post("/user", (request, response, next) => {
   })
     .then(user => response.send(user))
     .catch(error => next(error));
+});
+
+router.post("/user/login", async (request, response) => {
+  const { email, password } = request.body;
+  const user = await User.findOne({ where: { email: email } });
+  const passwordValid = bcrypt.compareSync(password, user.password);
+
+  if (passwordValid) {
+    // TODO: const token = jwt.sign({ id: user.id }, secret, { expiresIn: "2h" });
+    const token = toJWT({ id: user.id });
+    return response.status(200).send({ token: token });
+  }
 });
 module.exports = router;
