9620df260f8e1f22af42add5dc47ea75ea25f052
jhhest <janvanhest@outlook.com>
2020-02-17 12:08:30 +0100

    Added POST /user to create a new user

--- routes/user/router.js
+++ routes/user/router.js
@@ -1,12 +1,27 @@
 const { Router } = require("express");
-
 const router = new Router();
 
 const User = require("./model");
 
-router.get("/user", (request, response) =>
+router.get("/user", (request, response, next) =>
   User.findAll()
-  .then( user=> response.send(user)) //  TODO: Do do we need user.json? 
-  .catch(error => next(error))
+    .then(user => response.send(user)) //  TODO: Do do we need user.json?
+    .catch(error => next(error))
 );
+
+// http :5000/user username=jan email=jan@vanhest.work password=password
+router.post("/user", (request, response, next) => {
+  // console.log("request body", request.body)
+  console.log(
+    `
+    ----------
+    request = 
+    ----------
+    ${request}/n`,
+    request.body
+  );
+  User.create(request.body)
+    .then(user => response.send(user))
+    .catch(error => next(error));
+});
 module.exports = router;
