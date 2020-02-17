f8c3c88f2fa74b121f1112ee1bf908610168b5af
jhhest <janvanhest@outlook.com>
2020-02-17 11:13:30 +0100

    Create User table // model.js in /router/user

--- routes/user/router.js
+++ routes/user/router.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
+
 const router = new Router();
+
+const User = require("./model");
 
 router.get("/user", (request, response) =>
   response.send("This is the user endpoint! ")
