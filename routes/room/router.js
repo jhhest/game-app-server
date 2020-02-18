bce29612982862b64206f8294c6f67c96ce60152
jhhest <janvanhest@outlook.com>
2020-02-18 16:41:46 +0100

    Changed async await construction in /room endpoint

--- routes/room/router.js
+++ routes/room/router.js
@@ -4,7 +4,7 @@ const router = new Router();
 const Room = require("./model.js");
 
 // FIXME: Get all the rooms
-router.get("/room", async (request, response) => await response.send(Room.findAll()));// FIXME: check if async await is working. 
+router.get("/room", async (request, response) => response.send(await Room.findAll()));
 
 // Create a new room. 
 router.post("/room/new", auth, async (request, response) => {
